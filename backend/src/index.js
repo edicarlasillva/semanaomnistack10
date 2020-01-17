50b5647afbeb379fdfb509e7c5d257aa5e2bc440
Edicarla Sillva <edicarlasillva@gmail.com>
2020-01-17 14:29:44 -0300

    Update

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://edicarlasillva:Carlinha123@cluster0-7rpb4.mongodb.net/week10?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const axios = require('axios');
+const routes = Router();
+
+routes.post('/devs', async (request, response) => {
+  const { github_username } = request.body;
+
+  const apiResponse = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+  console.log(apiResponse.data);
+  return response.json({ message: 'Hello Omnistack' });
+});
+
+module.exports = routes;
